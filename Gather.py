0be985cd8ddc37b3c3fd48063f74d4c76b58b2e4
Brandon Stine <brandon.stine99@gmail.com>
2023-09-27 01:47:25 -0400

    commit

--- Gather.py
+++ Gather.py
@@ -1,47 +1,71 @@
 import requests
 import csv
 
-# Base URL for the latest available ACS 5-year estimates
+def get_variable_labels(variable_codes):
+    """
+    Fetch the corresponding labels for the provided variable codes from the Census API.
+    
+    Parameters:
+    - variable_codes (list): List of variable codes to fetch labels for.
+    
+    Returns:
+    - dict: A dictionary mapping variable codes to their respective labels.
+    """
+    url = "https://api.census.gov/data/2021/acs/acs5/variables.json"
+    response = requests.get(url)
+    data = response.json()
+    
+    labels = {}
+    for code in variable_codes:
+        if code in data["variables"]:
+            label = data["variables"][code]["label"]
+            # Custom replacements based on the specific labels
+            if label == "Estimate!!Total:":
+                label = "Estimated Population"
+            elif label == "Estimate!!Median age --!!Total:":
+                label = "Median Age"
+            elif "Median household income" in label:
+                label = "Median Household Income"
+            else:
+                label = label.replace("Estimate!!Total:!!", "").replace("Estimate!!", "")
+            labels[code] = label
+        else:
+            print(f"Warning: {code} not found in the API response.")
+            
+    return labels
+
 base_url = "https://api.census.gov/data/2021/acs/acs5"
+variables_list = ["NAME","B01001_001E","B01002_001E","B19013_001E","B24022_060E","B19001B_014E"]
+variables_labels = get_variable_labels(variables_list)
+variables = ",".join(variables_list)
 
-# Variables to fetch:
-variables = "NAME,B01001_001E,B01002_001E,B19013_001E"
-
-# Florida state code
 florida_code = "12"
-
-# Fetch all county codes for Florida
 county_url = f"{base_url}?get=NAME&for=county:*&in=state:{florida_code}&key=39a6ddc087c074ec8657a6df51e84575df9809f1"
 county_response = requests.get(county_url)
 counties = [item[2] for item in county_response.json()[1:]]
 
-# Define a local file path to save the data
-file_path = 'florida_county_all_block_group_data_2021.csv'
-
+file_path = 'data.csv'
 with open(file_path, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
-    # Updated header
-    writer.writerow(["Block Group", "Census Tract", "County", "Total Population", "Median Age", "Median Household Income", "State Code", "County Code", "Block Group Code"])
-
+    header = ["Block Group", "Census Tract", "County"] + list(variables_labels.values()) + ["State Code", "County Code","Block Group Code"]
+    writer.writerow(header)
     for county_code in counties:
-        # Constructing the API request URL for block groups within the county
         api_url = f"{base_url}?get={variables}&for=block group:*&in=state:{florida_code}&in=county:{county_code}&key=39a6ddc087c074ec8657a6df51e84575df9809f1"
         response = requests.get(api_url)
-        
         if response.status_code == 200:
-            data = response.json()[1:]
-            
-            # Process and write the data
+            try:
+                data = response.json()[1:]
+            except ValueError:
+                continue
             for row in data:
-                # Splitting the NAME field
                 parts = row[0].split(", ")
+                if len(parts) < 3:
+                    continue
                 block_group = parts[0]
                 census_tract = parts[1]
                 county = parts[2]
-                
-                # Writing the processed data
-                writer.writerow([block_group, census_tract, county, row[1], row[2], row[3], row[4], row[5], row[6]])
-
+                # Exclude the last value (row[-1]) and keep the rest
+                writer.writerow([block_group, census_tract, county] + row[1:-1])
         else:
             print(f"Failed to retrieve data for Florida county:{county_code}. Error: {response.text}")
 
---
+++ map.py
@@ -0,0 +1,45 @@
+import geopandas as gpd
+import pandas as pd
+import folium
+from folium.plugins import FastMarkerCluster
+
+# Load the block groups shapefile
+blocks = gpd.read_file("tl_rd22_12_bg.shp")
+blocks['geometry'] = blocks['geometry'].simplify(tolerance=0.001)
+data = pd.read_csv("data.csv")
+
+# Convert each column to string and ensure correct zero-padding
+data['State Code'] = data['State Code'].astype(str).str.zfill(2)
+data['County Code'] = data['County Code'].astype(str).str.zfill(3)
+data['Census Tract'] = data['Census Tract'].str.replace('Census Tract ', '')
+data['Block Group'] = data['Block Group'].str.replace('Block Group ', '').astype(str)
+
+# Create the new 'GEOID_new' in the data DataFrame
+data['GEOID_new'] = data['State Code'] + data['County Code'] +  data['Census Tract'].str.replace('Census Tract ', '').str.replace('.', '').astype(str).str.zfill(6) + data['Block Group']
+
+# Merge the geospatial data with your data based on the new GEOID
+merged = blocks.merge(data, left_on='GEOID', right_on='GEOID_new', how='left')
+
+# Filter out invalid income values
+merged = merged[merged['Median Household Income'] > 0]
+
+
+# Create the map centered on the data
+m = folium.Map(location=[merged['geometry'].centroid.y.mean(), 
+                         merged['geometry'].centroid.x.mean()], 
+               zoom_start=10, tiles="cartodb positron")
+# Automatically generate fields and aliases based on CSV headers
+tooltip_fields = data.columns
+tooltip_aliases = [field.replace('_', ' ').title() + ':' for field in tooltip_fields]
+
+# Add block groups to the map with interactivity
+folium.GeoJson(merged, 
+               tooltip=folium.GeoJsonTooltip(fields=tooltip_fields.tolist(), aliases=tooltip_aliases, localize=True),
+               style_function=lambda x: {'fillColor': '#ffaf00', 'color': '#00ff22', 'weight': 0.1},
+               smooth_factor=2.0).add_to(m)
+
+# Add layer control to toggle roads and landmarks
+folium.LayerControl().add_to(m)
+
+# Save the map to an HTML file
+m.save("interactive_map.html")

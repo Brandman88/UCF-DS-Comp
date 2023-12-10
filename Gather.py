9fe77395190e8687fb04a23405649fb5a60e36e1
Jason <100395902+jascnrod@users.noreply.github.com>
2023-12-10 02:43:01 -0500

    jason type comennts

--- Gather.py
+++ Gather.py
@@ -11,14 +11,16 @@ def get_variable_labels(variable_codes):
     Returns:
     - dict: A dictionary mapping variable codes to their respective labels.
     """
+    #url contains census data. Formated in dictionary style. Give it a search
     url = "https://api.census.gov/data/2021/acs/acs5/variables.json"
-    response = requests.get(url)
-    data = response.json()
+    response = requests.get(url) # get(url) function sends the request to that URL and returns an HTTP response object
+    data = response.json() # contains a Python data structure that corresponds to the JSON data found in the response.
     
-    labels = {}
+    labels = {} #initializing empty dictionary
+    # loop to retrieve json data and shove them into labels dictionary.
     for code in variable_codes:
-        if code in data["variables"]:
-            label = data["variables"][code]["label"]
+        if code in data["variables"]:#searches for the string variables in data
+            label = data["variables"][code]["label"]#code is a unique identifier for a person 
             # Custom replacements based on the specific labels
             if label == "Estimate!!Total:":
                 label = "Estimated Population"
@@ -35,23 +37,30 @@ def get_variable_labels(variable_codes):
     return labels
 
 base_url = "https://api.census.gov/data/2021/acs/acs5"
-variables_list = ["NAME","B01001_001E","B01002_001E","B19013_001E","B24022_060E","B19001B_014E"]
+variables_list = ["NAME","B01001_001E","B01002_001E","B19013_001E","B24022_060E","B19001B_014E"]# why are we only looking for specific variables. this only accounts for a fraction of them. 
 variables_labels = get_variable_labels(variables_list)
-variables = ",".join(variables_list)
+variables = ",".join(variables_list)#seperates everything with commas
 
 florida_code = "12"
+#This variable contains the URL for making an API request to retrieve data for specific variables at the county level in Florida. It includes parameters like get=NAME, 
+# which indicates that you want to retrieve the county names, for=county:*, which specifies that you want data at the county level, 
+# and in=state:12, which restricts the query to Florida. It also includes an API key for authentication.
 county_url = f"{base_url}?get=NAME&for=county:*&in=state:{florida_code}&key=39a6ddc087c074ec8657a6df51e84575df9809f1"
 county_response = requests.get(county_url)
+#county response is list that gives you all of the counties, their names the state, and county codes
+#[["NAME","state","county"],["Alachua County, Florida","12","001"],["Baker County, Florida","12","003"],...]
 counties = [item[2] for item in county_response.json()[1:]]
 
 file_path = 'data.csv'
 with open(file_path, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
-    header = ["Block Group", "Census Tract", "County"] + list(variables_labels.values()) + ["State Code", "County Code","Block Group Code"]
+    header = ["Block Group", "Census Tract", "County"] + list(variables_labels.values()) + ["State Code", "County Code","Block Group Code"]# all one row
     writer.writerow(header)
     for county_code in counties:
+        # updates url for specifice census data
         api_url = f"{base_url}?get={variables}&for=block group:*&in=state:{florida_code}&in=county:{county_code}&key=39a6ddc087c074ec8657a6df51e84575df9809f1"
         response = requests.get(api_url)
+        #brute force read and write the data.
         if response.status_code == 200:
             try:
                 data = response.json()[1:]

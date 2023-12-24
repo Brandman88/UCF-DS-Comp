c70450da91eb3384fff08d70f93f8782e0f29b4f
Brandon Stine <brandon.stine99@gmail.com>
2023-12-23 21:02:24 -0500

    increasing amount that can be displayed

--- map.py
+++ map.py
@@ -3,6 +3,63 @@ import pandas as pd
 import folium
 from folium.plugins import FastMarkerCluster
 
+# Define style functions
+def road_style_function(feature):
+    return {'color': 'grey', 'weight': 1}
+
+def pointlm_style_function(feature):
+    return {'fillColor': 'purple', 'color': 'purple', 'weight': 1, 'fillOpacity': 0.6}
+
+
+def block_style_function(feature):
+    return {'fillColor': '#ffaf00', 'color': 'blue', 'weight': 2, 'fillOpacity': 0.5, 'dashArray': '5, 5'}
+
+def place_style_function(feature):
+    return {
+        'fillColor': 'green',
+        'color': 'darkgreen',
+        'weight': 2,
+        'fillOpacity': 0.6,
+        'dashArray': '5, 5'
+    }
+
+def tract_style_function(feature):
+    return {
+        'fillColor': 'orange',
+        'color': 'black',
+        'weight': 2,
+        'fillOpacity': 0.6,
+        'dashArray': '5, 5'
+    }
+
+def cousub_style_function(feature):
+    return {
+        'fillColor': 'red',
+        'color': 'purple',
+        'weight': 2,
+        'fillOpacity': 0.6,
+        'dashArray': '5, 5'
+    }
+
+
+# Custom callback function to create popups
+def callback(row):
+    return ('<strong>Name:</strong> {name}').format(name=row[2])
+
+
+
+
+
+
+
+
+def filter_with_sindex(gdf, gdf_index, other_geom):
+    possible_matches_index = list(gdf_index.intersection(other_geom.bounds))
+    possible_matches = gdf.iloc[possible_matches_index]
+    precise_matches = possible_matches[possible_matches.intersects(other_geom)]
+    return precise_matches
+
+# Define the projected CRS and the field name mapping
 projected_crs = 'EPSG:4269'
 
 field_name_mapping = {
@@ -17,16 +74,58 @@ field_name_mapping = {
 # Load the block groups and roads shapefiles
 blocks = gpd.read_file("tl_rd22_12_bg.shp")
 roads = gpd.read_file("tl_rd22_12_prisecroads.shp")
+places = gpd.read_file("tl_rd22_12_place.shp")
+pointlm = gpd.read_file("tl_rd22_12_pointlm.shp")
+tracts = gpd.read_file("tl_rd22_12_tract.shp")
+cousub = gpd.read_file("tl_rd22_12_cousub.shp")
 
 print("Original CRS:", blocks.crs)  # It should print EPSG:4269
+
+#print(blocks['TYPE'].unique())
+#print(roads['TYPE'].unique())
+#print(places['TYPE'].unique())
+print(pointlm)
+#print(tracts['TYPE'].unique())
+#print(cousub['TYPE'].unique())
+
+
+# Simplify geometries
+#blocks['geometry'] = blocks['geometry'].simplify(tolerance=0.001)
+#places['geometry'] = places['geometry'].simplify(tolerance=0.001)
+#pointlm['geometry'] = pointlm['geometry'].simplify(tolerance=0.001)
+#tracts['geometry'] = tracts['geometry'].simplify(tolerance=0.001)
+#cousub['geometry'] = cousub['geometry'].simplify(tolerance=0.001)
+
+
+# Get the column names for each GeoDataFrame
+tracts_columns = tracts.columns.tolist()
+places_columns = places.columns.tolist()
+cousub_columns = cousub.columns.tolist()
+pointlm_columns = pointlm.columns.tolist()
+
+print("Tracts columns:", tracts_columns)
+print("Places columns:", places_columns)
+print("Cousub columns:", cousub_columns)
+print("PointLM columns:", pointlm_columns)
+
+
 
 # Reproject the geometries to the projected CRS for accurate calculations
 blocks_projected = blocks.to_crs(projected_crs)
 roads_projected = roads.to_crs(projected_crs)
+places_projected = places.to_crs(projected_crs)
+pointlm_projected = pointlm.to_crs(projected_crs)
+tracts_projected = tracts.to_crs(projected_crs)
+cousub_projected = cousub.to_crs(projected_crs)
 
+# Create spatial indexes for faster querying
+blocks_index = blocks_projected.sindex
+roads_index = roads_projected.sindex
+places_index= places_projected.sindex
+pointlm_index= pointlm_projected.sindex
+tracts_index= tracts_projected.sindex
+cousub_index= cousub_projected.sindex
 
-# Simplify geometries
-blocks['geometry'] = blocks['geometry'].simplify(tolerance=0.001)
 
 # Load your data from the CSV
 data = pd.read_csv("data.csv")
@@ -54,19 +153,30 @@ block_buffer = filtered_blocks_projected['geometry'].buffer(buffer_distance)
 unified_buffer = block_buffer.unary_union
 
 # Filter roads that intersect with the buffer area (in the projected CRS)
-filtered_roads_projected = roads_projected[roads_projected.intersects(unified_buffer)]
+filtered_roads_projected = filter_with_sindex(roads_projected, roads_index, unified_buffer)
+filtered_places_projected = filter_with_sindex(places_projected, places_index, unified_buffer)
+filtered_pointlm_projected = filter_with_sindex(pointlm_projected, pointlm_index, unified_buffer)
+filtered_tracts_projected = filter_with_sindex(tracts_projected, tracts_index, unified_buffer)
+filtered_cousub_projected = filter_with_sindex(cousub_projected, cousub_index, unified_buffer)
+
 
 # Reproject the filtered geometries back to the original geographic CRS for mapping
 filtered_blocks_geo = filtered_blocks_projected.to_crs('EPSG:4269')
 filtered_roads_geo = filtered_roads_projected.to_crs('EPSG:4269')
+filtered_places_geo = filtered_places_projected.to_crs('EPSG:4269')
+filtered_pointlm_geo = filtered_pointlm_projected.to_crs('EPSG:4269')
+filtered_tracts_geo = filtered_tracts_projected.to_crs('EPSG:4269')
+filtered_cousub_geo = filtered_cousub_projected.to_crs('EPSG:4269')
+
+# Convert the GeoDataFrame of filtered places into a list of [lat, lon] pairs
 
 
-# Define style functions
-def road_style_function(feature):
-    return {'color': 'grey', 'weight': 1}
+landmarks_points = filtered_pointlm_geo[['geometry']].copy()
+landmarks_points['geometry'] = landmarks_points['geometry'].apply(lambda geom: [geom.y, geom.x])
+landmarks_points_list = landmarks_points['geometry'].tolist()
+#landmarks_points_list = filtered_pointlm_geo.apply( lambda row: [row.geometry.y, row.geometry.x, row['FULLNAME']], axis=1).tolist()
 
-def block_style_function(feature):
-    return {'fillColor': '#ffaf00', 'color': 'blue', 'weight': 2, 'fillOpacity': 0.5, 'dashArray': '5, 5'}
+
 
 
 if not filtered_blocks_geo.empty:
@@ -74,7 +184,9 @@ if not filtered_blocks_geo.empty:
 
     # Create the map centered on the data
     m = folium.Map(location=map_center, zoom_start=10, tiles="cartodb positron")
+    
 
+    
     # Fields to always exclude
     always_exclude = ['STATEFP', 'COUNTYFP', 'TRACTCE', 'BLKGRPCE', 'GEOID', 'NAMELSAD', 'MTFCC', 'FUNCSTAT', 'geometry']
 
@@ -125,13 +237,61 @@ if not filtered_blocks_geo.empty:
         filtered_blocks_geo,
         style_function=lambda x: {'fillColor': '#ffaf00', 'color': 'blue', 'weight': 2, 'fillOpacity': 0.5, 'dashArray': '5, 5'},
         tooltip=folium.GeoJsonTooltip(fields=tooltip_fields, aliases=tooltip_aliases, localize=True),
-        name="Block Groups"  # Custom name for the block groups layer
+        name="Block Groups",  # Custom name for the block groups layer
+        show=False
     ).add_to(m)
     
+    if not filtered_places_geo.empty:
+        folium.GeoJson(
+            filtered_places_geo,
+            style_function=place_style_function,
+            name="Places",
+            show=False,
+            tooltip=folium.GeoJsonTooltip(
+                fields=['NAMELSAD', 'ALAND', 'AWATER', 'INTPTLAT', 'INTPTLON'],
+                aliases=['Place Name:', 'Area of Land:', 'Area of Water:', 'Latitude:', 'Longitude:']
+            )
+        ).add_to(m)
+
+    if not filtered_pointlm_geo.empty:
+        FastMarkerCluster(data=landmarks_points_list, name="Landmarks", show=False).add_to(m)
+    '''
+   if not filtered_pointlm_geo.empty:
+        FastMarkerCluster(
+            data=landmarks_points_list,
+            name="Landmarks",
+            show=False,
+            callback=callback
+        ).add_to(m)
+    '''
+
+
+    if not filtered_tracts_geo.empty:
+        folium.GeoJson(
+            filtered_tracts_geo,
+            style_function=tract_style_function,
+            name="Census Tracts",
+            show=False,
+            tooltip=folium.GeoJsonTooltip(
+                fields=['NAMELSAD', 'ALAND', 'AWATER', 'INTPTLAT', 'INTPTLON'],
+                aliases=['Tract Name:', 'Area of Land:', 'Area of Water:', 'Latitude:', 'Longitude:']
+            )
+        ).add_to(m)
+
+    if not filtered_cousub_geo.empty:
+        folium.GeoJson(
+            filtered_cousub_geo,
+            style_function=cousub_style_function,
+            name="County Subdivisions",
+            show=False,
+            tooltip=folium.GeoJsonTooltip(
+                fields=['NAMELSAD', 'ALAND', 'AWATER', 'INTPTLAT', 'INTPTLON'],
+                aliases=['Subdivision Name:', 'Area of Land:', 'Area of Water:', 'Latitude:', 'Longitude:']
+            )
+        ).add_to(m)
+    
     # Add filtered roads to the map
-    folium.GeoJson(filtered_roads_geo,
-               style_function=road_style_function,
-               name="Filtered Roads").add_to(m)
+    folium.GeoJson(filtered_roads_geo,style_function=road_style_function,name="Filtered Roads", show=False).add_to(m)
 
     # Add layer control to toggle layers
     folium.LayerControl().add_to(m)
